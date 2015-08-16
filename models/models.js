2518676a0f019022c95054af7114361798daeb9c
Ruben <ruben.villagarcia@gmail.com>
2015-08-16 17:12:11 +0200

    Crear comentarios

--- models/models.js
+++ models/models.js
@@ -28,7 +28,16 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 //Importar la definición de la tabla Quiz en quiz.js.
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
+//Importar definición de la tabla Comment
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; //exportar definición de la tabla Quiz.
+
+exports.Comment = Comment;
 
 //Crea e inicializa tabla de preguntas en BBDD.
 sequelize.sync().then(function(){
